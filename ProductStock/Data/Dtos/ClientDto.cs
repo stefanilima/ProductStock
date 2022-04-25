1fb977c56bef0ddba7da9bfdc5f04cfef30c6d19
stefanilima <stefanisilvadelima@hotmail.com>
2022-04-25 16:16:26 -0300

    Created endpoint client

---
+++ ProductStock/Data/Dtos/ClientDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductStock.Data.Dtos
+{
+    public class ClientDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name must contain a maximum of 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "BirthDate is required")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(100, ErrorMessage = "Email must contain a maximum of 100 characters")]
+        public string Email { get; set; }
+    }
+}
